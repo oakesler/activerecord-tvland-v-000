cf4687194ae0e3808d68969c48e639ba6c6d7028
Olivia Kesler <oakesler@protonmail.ch>
2019-12-14 05:31:05 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_day_genre_season_to_shows.rb
+++ db/migrate/007_add_day_genre_season_to_shows.rb
@@ -1,10 +1,8 @@
 class AddDayGenreSeasonToShows < ActiveRecord::Migration[5.1]
   
   def change
-    add_column :shows do |t|
-      t.string :day
-      t.string :genre
-      t.string :season
-    end
+    add_column :shows, :day, :string
+    add_column :shows, :genre, :string
+    add_column :shows, :season, :string
   end
 end
